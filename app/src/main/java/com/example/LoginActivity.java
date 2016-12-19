68e00efc4d35f8eee7cd225bb31bf0052a842598
seby-sbirna <seby.sbirna@gmail.com>
2016-12-19 01:10:32 +0100

    BeFriendy v1.0.2
    - Introduced a function to check at login and register whether the
    device has internet connection or not
    
    Known new bugs:
    - A slight delay of up to 1 second is experienced when logging in or
    registering, due to waiting for a network task to be executed in the
    background

--- app/src/main/java/com/example/LoginActivity.java
+++ app/src/main/java/com/example/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.example;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +21,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -91,26 +101,38 @@ public class LoginActivity extends AppCompatActivity {
                 String typedEmail = emailLogin.getText().toString();
                 String typedPassword = passwordLogin.getText().toString();
 
-                //TODO: Internet connection check by pinging google!
+                NetworkTesting networkTesting = new NetworkTesting();
 
-                //Fields Validation
-                if (typedEmail.isEmpty() || typedPassword.isEmpty()) {
-                    //For testing purposes if the fieldImageViews are empty it would log you with test@test.com
-                    typedEmail = "test@test.com";
-                    typedPassword = "test";
-                    SignInUser(typedEmail, typedPassword);
-                    Toast.makeText(getApplicationContext(), "Logged in with test@test.com", Toast.LENGTH_SHORT).show();
-//                    Toast.makeText(getApplicationContext(), "Please fill in both email and password!", Toast.LENGTH_LONG).show();
+                try {
+                    if (networkTesting.execute().get() == true) {
+                        //TODO: Internet connection check by pinging google!
 
-                } else if (!isValidEmail(typedEmail)) {
-                    Toast.makeText(getApplicationContext(), "The email is not valid!", Toast.LENGTH_SHORT).show();
-                }/* else if (typedPassword.length() < 6) {
-                    Toast.makeText(getApplicationContext(), "This password is too short", Toast.LENGTH_LONG).show(); }*/ //so it is easier to develop
-                else if (typedPassword.length() > 19) {
-                    Toast.makeText(getApplicationContext(), "This password is too long", Toast.LENGTH_SHORT).show();
-                } else {
-                    //check if the user is registered or not
-                    SignInUser(typedEmail, typedPassword);
+                        //Fields Validation
+                        if (typedEmail.isEmpty() || typedPassword.isEmpty()) {
+                            //For testing purposes if the fieldImageViews are empty it would log you with test@test.com
+                            typedEmail = "test@test.com";
+                            typedPassword = "test";
+                            SignInUser(typedEmail, typedPassword);
+                            Toast.makeText(getApplicationContext(), "Logged in with test@test.com", Toast.LENGTH_SHORT).show();
+                            //                    Toast.makeText(getApplicationContext(), "Please fill in both email and password!", Toast.LENGTH_LONG).show();
+
+                        } else if (!isValidEmail(typedEmail)) {
+                            Toast.makeText(getApplicationContext(), "The email is not valid!", Toast.LENGTH_SHORT).show();
+                        }/* else if (typedPassword.length() < 6) {
+                            Toast.makeText(getApplicationContext(), "This password is too short", Toast.LENGTH_LONG).show(); }*/ //so it is easier to develop
+                        else if (typedPassword.length() > 19) {
+                            Toast.makeText(getApplicationContext(), "This password is too long", Toast.LENGTH_SHORT).show();
+                        } else {
+                            //check if the user is registered or not
+                            SignInUser(typedEmail, typedPassword);
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Your device does not have an internet connection", Toast.LENGTH_LONG).show();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -154,4 +176,40 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Email or password incorrect", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
+    class NetworkTesting extends AsyncTask<String, Void, Boolean> {
+
+        private Exception exception;
+
+        protected Boolean doInBackground(String... xyz) {
+            if (isNetworkAvailable()) {
+                try {
+                    HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
+                    urlc.setRequestProperty("User-Agent", "Test");
+                    urlc.setRequestProperty("Connection", "close");
+                    urlc.setConnectTimeout(1500);
+                    urlc.connect();
+                    return (urlc.getResponseCode() == 200);
+                } catch (IOException e) {
+                    Log.e("LoginActivity", "Error checking internet connection", e);
+                }
+            } else {
+                Log.d("LoginActivity", "No network available!");
+            }
+            return false;
+        }
+
+
+        private boolean isNetworkAvailable() {
+            ConnectivityManager connectivityManager
+                    = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null;
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/RegistrationActivity.java
+++ app/src/main/java/com/example/RegistrationActivity.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +22,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -87,50 +96,59 @@ public class RegistrationActivity extends AppCompatActivity {
                 String chosenPassword = regPassword.getText().toString();
                 String repeatChosenPassword = regRepeatPassword.getText().toString();
 
-                if (chosenUserName.isEmpty() || chosenEmail.isEmpty() || chosenPassword.isEmpty() || repeatChosenPassword.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please fill in all the fields!", Toast.LENGTH_LONG).show();
-                } else if (!chosenPassword.equals(repeatChosenPassword)) {
-                    Toast.makeText(getApplicationContext(), "The passwords does not match! Please input them again!", Toast.LENGTH_LONG).show();
-                }
-                // more if statements to validate registration...
-                else {
-                    // Code to register user in Database goes here...
-                    boolean userExists = false;
-                    for (DatabaseUser currUser : Player.get().getUsers()) {
-                        if (chosenEmail.equals(currUser.getEmail())) {
-                            Toast.makeText(getApplicationContext(), "This email already exists", Toast.LENGTH_LONG).show();
-                            userExists = true;
-                            break;
+                NetworkTesting networkTesting = new NetworkTesting();
+
+                try {
+                    if (networkTesting.execute().get() == true) {
+                        if (chosenUserName.isEmpty() || chosenEmail.isEmpty() || chosenPassword.isEmpty() || repeatChosenPassword.isEmpty()) {
+                            Toast.makeText(getApplicationContext(), "Please fill in all the fields!", Toast.LENGTH_LONG).show();
+                        } else if (!chosenPassword.equals(repeatChosenPassword)) {
+                            Toast.makeText(getApplicationContext(), "The passwords does not match! Please input them again!", Toast.LENGTH_LONG).show();
                         }
-                        if (chosenUserName.equals(currUser.getUserName())) {
-                            Toast.makeText(getApplicationContext(), "This username already exists", Toast.LENGTH_LONG).show();
-                            userExists = true;
-                            break;
+                        // more if statements to validate registration...
+                        else {
+                            // Code to register user in Database goes here...
+                            boolean userExists = false;
+                            for (DatabaseUser currUser : Player.get().getUsers()) {
+                                if (chosenEmail.equals(currUser.getEmail())) {
+                                    Toast.makeText(getApplicationContext(), "This email already exists", Toast.LENGTH_LONG).show();
+                                    userExists = true;
+                                    break;
+                                }
+                                if (chosenUserName.equals(currUser.getUserName())) {
+                                    Toast.makeText(getApplicationContext(), "This username already exists", Toast.LENGTH_LONG).show();
+                                    userExists = true;
+                                    break;
+                                }
+                            }
+                            if (!userExists) {
+                                newUser = new DatabaseUser();
+                                newUser.setEmail(chosenEmail);
+                                newUser.setUserName(chosenUserName);
+                                newUser.setPassword(chosenPassword);
+                                newUser.setUserId(UUID.randomUUID().toString());
+                                newUser.setTokenId(0);
+                                mDatabase.child("users").child(newUser.getUserId()).setValue(newUser);
+
+                                Player.get().setUserName(newUser.getUserName());
+                                Player.get().setEmailAddress(newUser.getEmail());
+                                Player.get().setUserId(newUser.getUserId());
+
+                                //Log.d("AUTH", Player.get().getUsers().contains());
+                                Toast.makeText(getApplicationContext(), "User registered", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                                finish(); //so the user can't go back;
+                            }
                         }
+                    } else {
+                        Toast.makeText(RegistrationActivity.this, "Your device does not have an internet connection", Toast.LENGTH_LONG).show();
                     }
-                    if (!userExists) {
-                        newUser = new DatabaseUser();
-                        newUser.setEmail(chosenEmail);
-                        newUser.setUserName(chosenUserName);
-                        newUser.setPassword(chosenPassword);
-                        newUser.setUserId(UUID.randomUUID().toString());
-                        newUser.setTokenId(0);
-                        mDatabase.child("users").child(newUser.getUserId()).setValue(newUser);
-
-                        Player.get().setUserName(newUser.getUserName());
-                        Player.get().setEmailAddress(newUser.getEmail());
-                        Player.get().setUserId(newUser.getUserId());
-
-                        //Log.d("AUTH", Player.get().getUsers().contains());
-                        Toast.makeText(getApplicationContext(), "User registered", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                        startActivity(intent);
-                        finish(); //so the user can't go back;
-                    }
-
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
                 }
-
-
             }
         });
     }
@@ -143,4 +161,34 @@ public class RegistrationActivity extends AppCompatActivity {
         }
     }
 
+    class NetworkTesting extends AsyncTask<String, Void, Boolean> {
+
+        private Exception exception;
+
+        protected Boolean doInBackground(String... xyz) {
+            if (isNetworkAvailable()) {
+                try {
+                    HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
+                    urlc.setRequestProperty("User-Agent", "Test");
+                    urlc.setRequestProperty("Connection", "close");
+                    urlc.setConnectTimeout(1500);
+                    urlc.connect();
+                    return (urlc.getResponseCode() == 200);
+                } catch (IOException e) {
+                    Log.e("LoginActivity", "Error checking internet connection", e);
+                }
+            } else {
+                Log.d("LoginActivity", "No network available!");
+            }
+            return false;
+        }
+
+
+        private boolean isNetworkAvailable() {
+            ConnectivityManager connectivityManager
+                    = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null;
+        }
+    }
 }
